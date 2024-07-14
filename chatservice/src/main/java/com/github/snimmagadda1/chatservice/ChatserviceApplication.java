0c5762b49ad5e8448db7ee39cd70fc0871b63390
snimmagadda1 <saiguy@me.com>
2024-07-14 16:06:47 -0400

    Remove not needed client lib

--- chatservice/src/main/java/com/github/snimmagadda1/chatservice/ChatserviceApplication.java
+++ chatservice/src/main/java/com/github/snimmagadda1/chatservice/ChatserviceApplication.java
@@ -1,6 +1,9 @@
 package com.github.snimmagadda1.chatservice;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,14 @@ public class ChatserviceApplication {
   @RequestMapping("/user")
   public Principal user(Principal user) {
     return user;
+  }
+
+  @RequestMapping("/resource")
+  public Map<String, Object> home() {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello World");
+    return model;
   }
 
   public static void main(String[] args) {
