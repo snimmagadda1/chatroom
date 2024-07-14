5e99c33a69ec2373c9e54982d4af376ee06b1d08
snimmagadda1 <saiguy@me.com>
2024-07-14 16:31:33 -0400

    Revert "Remove not needed client lib"
    This reverts commit 0c5762b49ad5e8448db7ee39cd70fc0871b63390.

--- chatservice/build.gradle.kts
+++ chatservice/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-	// implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
--- chatservice/src/main/java/com/github/snimmagadda1/chatservice/config/SecurityConfig.java
+++ chatservice/src/main/java/com/github/snimmagadda1/chatservice/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -39,7 +43,22 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             (requests) ->
                 requests.requestMatchers("/login").permitAll().anyRequest().authenticated())
+        .oauth2Login(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  public ClientRegistrationRepository clientRepository() {
+    ClientRegistration clientReg = clientRegistration();
+    return new InMemoryClientRegistrationRepository(clientReg);
+  }
+
+  private ClientRegistration clientRegistration() {
+    return CommonOAuth2Provider.GITHUB
+        .getBuilder("github")
+        .clientId("TODO")
+        .clientSecret("TODO")
+        .build();
   }
 }
