2e8fe1bfd311f186a9724772ca2e67b90b3d65fd
snimmagadda1 <saiguy@me.com>
2024-07-18 17:01:46 -0400

    Basic auth for integrated development while lazy

--- social-ui/src/app/app.component.ts
+++ social-ui/src/app/app.component.ts
@@ -6,8 +6,17 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'social-ui';
+
+  constructor() {
+    const testUser = 'sai@example.com';
+    const testPass = '12345';
+    window.sessionStorage.setItem(
+      'userdetails',
+      JSON.stringify({ username: testUser, password: testPass })
+    );
+  }
 }
