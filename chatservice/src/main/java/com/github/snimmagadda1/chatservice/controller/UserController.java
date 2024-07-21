a5408f2f63f78e8656bab628e023d9eb79dcfc59
snimmagadda1 <saiguy@me.com>
2024-07-21 19:11:17 -0400

    Base list users endpoint

--- chatservice/src/main/java/com/github/snimmagadda1/chatservice/controller/UserController.java
+++ chatservice/src/main/java/com/github/snimmagadda1/chatservice/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.github.snimmagadda1.chatservice.controller;
 
+import com.github.snimmagadda1.chatservice.model.AppConstants;
 import com.github.snimmagadda1.chatservice.service.KeycloakService;
 import java.util.List;
 import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,29 @@ public class UserController {
     this.keycloakService = keycloakService;
   }
 
-  @GetMapping("/roles")
-  public List<RoleRepresentation> getRoles() {
-    return keycloakService.getKeycloak().realm("chatappdev").roles().list();
+  @GetMapping("/users")
+  public List<UserRepresentation> getUsers() {
+    List<UserRepresentation> users =
+        keycloakService.getKeycloak().realm(AppConstants.CHAT_APP_REALM).users().list();
+    users.forEach(
+        user -> {
+          user.setRealmRoles(getRoles(user.getId()));
+        });
+
+    return users;
+  }
+
+  private List<String> getRoles(String userId) {
+    return keycloakService
+        .getKeycloak()
+        .realm(AppConstants.CHAT_APP_REALM)
+        .users()
+        .get(userId)
+        .roles()
+        .realmLevel()
+        .listEffective()
+        .stream()
+        .map(RoleRepresentation::getName)
+        .toList();
   }
 }
---
+++ chatservice/src/main/java/com/github/snimmagadda1/chatservice/model/AppConstants.java
@@ -0,0 +1,5 @@
+package com.github.snimmagadda1.chatservice.model;
+
+public class AppConstants {
+  public static final String CHAT_APP_REALM = "chatappdev";
+}
